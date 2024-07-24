bd009a8e62852eeebd830fe8ceed2f30c32a3c70
ArviS <alkannefee@gmail.com>
2024-07-24 16:35:51 +0330

    Add files via upload

---
+++ başlat.bat
@@ -0,0 +1,7 @@
+@echo off
+title arviis. - V14 Slashli Moderasyon
+cls
+:arviis 
+node app.js
+goto arviis
+pause
---
+++ test.bat
@@ -0,0 +1,7 @@
+@echo off
+title arviis. - V14 Slashli Moderasyon [TEST]
+cls
+:arviis2 
+npm run arviis:dev
+goto arviis2
+pause
